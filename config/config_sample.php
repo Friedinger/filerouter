dd9550c94a01cae63ddda27907ee5ca5e671dacd
Friedinger <71267611+Friedinger@users.noreply.github.com>
2023-05-29 17:30:21 +0200

    Remove database info from config

--- config/config_sample.php
+++ config/config_sample.php
@@ -6,9 +6,4 @@ final class Config
 	const PATH_PAGES = "/../pages/";
 	const PATH_MODULES = "/../modules/";
 	const PATH_ERROR = "/../modules/error.php";
-	const DB_HOST = "";
-	const DB_USERNAME = "";
-	const DB_PASSWORD = "";
-	const DB_NAME = "";
-	const DB_TABLE_USER = "";
 }
