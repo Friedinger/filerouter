61371e6fe29da204ff10522cd34dcb9dc786d4c4
Friedinger <info@friedinger.org>
2024-05-22 14:35:48 +0200

    🧑‍💻 Restructure config

--- config/config_sample.php
+++ config/config_sample.php
@@ -15,17 +15,17 @@ namespace FileRouter;
 
 final class Config
 {
-	//All paths are relative to the server root
+	// Paths (relative to the server root)
 	const PATH_PUBLIC = "/../public/"; // Path to the public folder
 	const PATH_MODULES = "/../modules/"; // Path to the modules folder
 	const PATH_HEAD = "/../modules/head.php"; // Path to the head file
 	const PATH_HEADER = "/../modules/header.php"; // Path to the header file
 	const PATH_FOOTER = "/../modules/footer.php"; // Path to the footer file
 	const PATH_ERROR = "/../modules/error.php"; // Path to the error file
-	const ALLOW_PAGE_PHP = true; // Allow to execute php code in pages. Warning: This can be a security risk if not handled carefully.
-	const SESSION_NAME = "FileRouter";
-	const ERROR_FATAL = "<h1>Error</h1><p>An error occurred in the request.</p><p>Please contact the webmaster</p>";
-	const ERROR_MESSAGE = [
+
+	// Errors
+	const ERROR_FATAL = "<h1>Error</h1><p>An error occurred in the request.</p><p>Please contact the webmaster</p>"; // Fatal error message if error page can't be loaded
+	const ERROR_MESSAGE = [ // Default error messages depending on error code
 		400 => "Bad Request",
 		401 => "Unauthorized",
 		403 => "Forbidden",
@@ -35,7 +35,23 @@ final class Config
 		"default" => "Error",
 	];
 
+	// Sessions
+	const SESSION = true; // Enable session handling
+	const SESSION_NAME = "FileRouter"; // Name of the session
+	const SESSION_COOKIE_PARAMS = [ // Session cookie parameters (https://www.php.net/manual/en/function.session-set-cookie-params.php)
+		"lifetime" => 0,
+		"path" => "/",
+		"domain" => "filerouter.home.localhost",
+		"secure" => false,
+		"httponly" => true,
+		"samesite" => "Strict",
+	];
+
+	// Page titles
 	const TITLE_PREFIX = "FileRouter";
 	const TITLE_SUFFIX = "";
 	const TITLE_SEPARATOR = " | ";
+
+	// Other
+	const ALLOW_PAGE_PHP = true; // Allow to execute php code in pages. Warning: This can be a security risk if not handled carefully.
 }
