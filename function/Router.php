77f00b006e7c509ffdee8dfb3ebd82b0c2393baa
Friedinger <info@friedinger.org>
2024-07-24 22:40:41 +0200

    🐛 Fix missing uri preparation at redirect

--- function/Router.php
+++ function/Router.php
@@ -57,6 +57,7 @@ class Router
 	 */
 	public static function redirect(string $uri): void
 	{
+		$uri = Misc::prepareUri($uri); // Prepare uri
 		header("Location: /$uri"); // Set location header
 		exit(); // Stop further execution
 	}
