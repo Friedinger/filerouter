89fb06d3a13a8993327d78e38ce0a103aa849411
Friedinger <info@friedinger.org>
2024-08-29 23:41:48 +0200

    💬 Change csrf sample page

--- public/csrf/index.php
+++ public/csrf/index.php
@@ -3,7 +3,7 @@
 	<p>Token should be used inside hidden input, text input just used for demo purpose.</p>
 
 	<form method="post">
-		<input type="text" name="csrf-token" value="<csrf-token />">
+		<input type="text" name="token" value="<csrf-token />">
 		<input type="submit">
 	</form>
 	<br>
@@ -14,9 +14,9 @@
 	use FileRouter\Request;
 
 	if ($_SERVER["REQUEST_METHOD"] === "POST") {
-		echo "Send token: " . Request::post("csrf-token") . "<br>";
+		echo "Send token: " . Request::post("token") . "<br>";
 		echo "Expected token: " . Request::session("csrf-token") . "<br>";
-		echo Misc::verifyCsrfToken(Request::post("csrf-token")) ? "Valid" : "Invalid";
+		echo Misc::verifyCsrfTokenPost() ? "Valid" : "Invalid";
 	}
 	?>
 </main>
